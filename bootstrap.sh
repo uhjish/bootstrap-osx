16b641935ebc783b2c80bb7784620cc4018e5b58
Ajish George <yell@aji.sh>
2018-02-20 17:54:31 -0500

    add git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh `"
-git clone https://github.com/uhjish/bootstrap.git 
+set -e -x
+#sh -c "`curl -fsSL https://raw.githubusercontent.com/uhjish/bootstrap-osx/master/bootstrap.sh `" |& tee ~/bootstrap-osx.log
+git config --global user.name "Ajish George"
+git config --global user.email yell@aji.sh
+git clone https://github.com/uhjish/bootstrap.git .bootstrap-osx
+cd .bootstrap-osx
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/versions/iterm2-beta
 brew install mas
